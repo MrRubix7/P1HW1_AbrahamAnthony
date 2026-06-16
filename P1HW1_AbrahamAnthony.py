9b91af11b738046592be2919e17ec56d5d47e90f
MrRubix7 <anthony.b.abraham@gmail.com>
2026-06-16 01:01:54 -0400

    Add files via upload
    The program requests user input, then displays a message welcoming them to class.

---
+++ P1HW1_AbrahamAnthony.py
@@ -0,0 +1,20 @@
+ # Your Name: Antony Abraham
+ # Date: 15 JUNE 2026
+ # Assignment Name: P2HW2
+ # A brief description of the project: Assignment tests student's knowledge of how to write code that collects information from user, processes information collected and display 
+
+print("-----Calculating Exponents-----\n")
+
+iBase = int(input("Enter and Integer as the base value: "))
+iExponent = int(input("Enter an integer as the exponent:"))
+iPower = iBase ** iExponent
+print("\n", iBase, "raised to the power of", iExponent, "is", iPower, "!!")
+
+print("\n-----Addition and Subtraction-----\n")
+
+iNum1 = int(input("Enter a starting integer: "))
+iNum2 = int(input("Enter an integer to add: "))
+iNum3 = int(input("Enter an integer to subtract: "))
+print("\n", iNum1, "+", iNum2, "-", iNum3, "is equal to", (iNum1 + iNum2 - iNum3))
+
+
